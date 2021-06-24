57783b0053ee2b7fee0c2bee79531f560f65458c
孟凡旭 <mengfanxu@qimai.cn>
2021-06-24 10:18:45 +0800

    实现选择

--- src/view/index/index.vue
+++ src/view/index/index.vue
@@ -3,100 +3,123 @@
     <p>原图：</p>
     <img
       class="img"
-      src="../../assets/img/1.jpeg"
+      src="../../assets/img/2.jpeg"
       alt=""
       srcset=""
     >
+    <select
+      v-model="selectVal"
+      @change="handleChange()"
+    >
+      <option
+        v-for="(item,index) in list"
+        :key="index"
+        :value="item.key"
+      >{{item.val}}</option>
+    </select>
     <p>上面图：</p>
-    <canvas
-      ref="canvas"
-    ></canvas>
+    <canvas ref="canvas"></canvas>
     <p> 下面图：</p>
-    <canvas
-      ref="canvasOrgin"
-    ></canvas>
+    <canvas ref="canvasOrgin"></canvas>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, reactive, toRefs } from "vue";
+const imgWidth = 200;
+      const imgHeight = 200;
 export default {
   components: {},
   setup() {
-    const canvas = ref(null);
-    const canvasOrgin = ref(null); 
+    const state = reactive({
+      selectVal: "normal",
+      canvas: null,
+      ctx: null,
+      canvasOrgin: null,
+      ctxOrgin: null,
+      list: [
+        {
+          key: "normal",
+          val: "正常",
+        },
+        {
+          key: "darken",
+          val: "变暗",
+        },
+        {
+          key: "lighten",
+          val: "变亮",
+        },
+      ],
+      data: null,
+      imageData: null,
+      originData: null,
+      imageOriginData: null
+    });
     onMounted(() => {
-      const ctx = canvas.value.getContext("2d");
-      const ctxOrgin = canvasOrgin.value.getContext("2d");
-      const imgWidth = 200;
-      const imgHeight = 200;
+      state.ctx = state.canvas.getContext("2d");
+      state.ctxOrgin = state.canvasOrgin.getContext("2d");
+      
       const dpi = window.devicePixelRatio || 1;
       console.log("dpi", dpi);
-      canvasOrgin.value.style.width = imgWidth + "px";
-      canvasOrgin.value.style.height = imgHeight + "px";
-      canvasOrgin.value.width = imgWidth * dpi;
-      canvasOrgin.value.height = imgHeight * dpi;
-      ctxOrgin.scale(dpi, dpi);
-
-      canvas.value.style.width = imgWidth + "px";
-      canvas.value.style.height = imgHeight + "px";
-      canvas.value.width = imgWidth * dpi;
-      canvas.value.height = imgHeight * dpi;
-      ctx.scale(dpi, dpi);
-
       
+      state.canvasOrgin.style.width = imgWidth + "px";
+      state.canvasOrgin.style.height = imgHeight + "px";
+      state.canvasOrgin.width = imgWidth * dpi;
+      state.canvasOrgin.height = imgHeight * dpi;
+      state.ctxOrgin.scale(dpi, dpi);
+
+      state.canvas.style.width = imgWidth + "px";
+      state.canvas.style.height = imgHeight + "px";
+      state.canvas.width = imgWidth * dpi;
+      state.canvas.height = imgHeight * dpi;
+      state.ctx.scale(dpi, dpi);
+
       const img = new Image();
       img.crossOrigin = "anonymous";
-      img.src = "/@img/1.jpeg";
+      img.src = "/@img/2.jpeg";
       img.onload = () => {
-        ctxOrgin.drawImage(img, 0, 0, imgWidth, imgHeight);
-        const imageOriginData = ctxOrgin.getImageData(0, 0, imgWidth, imgHeight);
-        const originData = imageOriginData.data;
+        state.ctxOrgin.drawImage(img, 0, 0, imgWidth, imgHeight);
+        state.imageOriginData = state.ctxOrgin.getImageData(0, 0, imgWidth, imgHeight);
+        state.originData = state.imageOriginData.data;
 
-        ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
-        const imageData = ctx.getImageData(0, 0, imgWidth, imgHeight);
-        const data = imageData.data;
-        // console.log(data);
-        // const pixel = () => {
-        //   const gap = 3;
-        //   const pow = 0.4;
-        //   const width = imgWidth * pow;
-        //   const height = imgHeight * pow;
-        //   for (let h = 0; h < height; h += gap) {
-        //     for (let w = 0; w < width; w += gap) {
-        //       const pos = (width * h + w) * 4;
-        //       const r = data[pos],
-        //         g = data[pos + 1],
-        //         b = data[pos + 2],
-        //         a = data[pos + 3];
-        //       ctx.fillStyle = `rgba(${r},${g},${b},${a})`;
-        //       ctx.fillRect(w, h, 4, 4);
-        //     }
-        //   }
-        // };
-        // pixel();
-        // const invert = ()=>{
-        //   for (let i = 0; i < data.length; i+=4) {
-        //     data[i] = 255 - data[i]
-        //     data[i+1] = 255 - data[i]
-        //     data[i+2] = 255 - data[i]
-        //   }
-        //   ctx.putImageData(imageData,0,0)
-        // }
-        // invert()
-        const darken = () => {
-
-        };
+        state.ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
+        state.imageData = state.ctx.getImageData(0, 0, imgWidth, imgHeight);
+        state.data = state.imageData.data;
       };
     });
+    // 变暗，取小值
+    const darken = (val) => {
+      state.ctxOrgin.fillStyle = val || "#000000";
+      state.ctxOrgin.fillRect(0, 0, imgWidth, imgHeight);
+      let data = state.data
+      let originData = state.originData
+      for (let i = 0; i < data.length; i++) {
+        data[i] = Math.min(data[i], originData[i]);
+        data[i + 1] = Math.min(data[i + 1], originData[i + 1]);
+        data[i + 2] = Math.min(data[i + 2], originData[i + 2]);
+      }
+      state.ctx.putImageData(state.imageData, 0, 0);
+    };
     // const canvas = document.getElementById("canvas");
     // const ctx = canvas.getContext("2d");
     // const img = new Image();
     // img.crossOrigin = "anonymous";
     // img.src = "/@img/1.jpeg";
+    const handleChange = () => {
+      switch (state.selectVal) {
+        case 'darken':
+          darken()
+          break;
+      
+        default:
+          console.log('hhas')
+          break;
+      }
+    };
     return {
-      canvas,
-      canvasOrgin
+      ...toRefs(state), 
+      handleChange,
     };
   },
 };
