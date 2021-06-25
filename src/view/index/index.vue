6e1fc33490899b7c8ed9b10030f3ca5885a7e493
孟凡旭 <mengfanxu@qimai.cn>
2021-06-25 09:38:27 +0800

    添加类型

--- src/view/index/index.vue
+++ src/view/index/index.vue
@@ -60,6 +60,26 @@ export default {
           key: "screen",
           val: "滤色",
         },
+        {
+          key: "colorBurn",
+          val: "颜色加深",
+        },
+        {
+          key: "colorDodge",
+          val: "颜色减淡",
+        },
+        {
+          key: "linearBurn",
+          val: "线形加深",
+        },
+        {
+          key: "linearDodge",
+          val: "线形减淡",
+        },
+        {
+          key: "softLight",
+          val: "柔光",
+        },
       ],
       data: null,
       imageData: null,
@@ -132,17 +152,60 @@ export default {
     };
     state.ways = {
       normal: normal,
+      // 变暗
+      // B<=A 则 C=B B>=A 则 C=A
       darken: (a, b) => {
         return Math.min(a, b);
       },
+      // 变亮
+      // B<=A 则 C=A B>A 则 C=B
       lighten: (a, b) => {
         return Math.max(a, b);
       },
+      //  正片叠底
+      // C=(A×B)/255
       multiply: (a, b) => {
         return parseInt((a * b) / 255);
       },
+      //  滤色
+      // C=255-(A反相×B反相)/255
       screen: (a, b) => {
         return 255 - parseInt((reverse(a) * reverse(b)) / 255);
+      },
+      //  颜色加深
+      // C=A-(A反相×B反相)/B
+      colorBurn: (a, b) => {
+        return a - parseInt((reverse(a) * reverse(b)) / 255);
+      },
+      // 颜色减淡
+      // C=A+(A×B)/B反相
+      colorDodge: (a, b) => {
+        return a + parseInt((a * b) / reverse(b));
+      },
+      // 线形加深
+      // C=A+B-255
+      linearBurn: (a, b) => {
+        return a + b - 255;
+      },
+      // 线形减淡
+      // C=A+B
+      linearDodge: (a, b) => {
+        return a + b;
+      },
+      // 柔光
+      // B<=128 则 C=(A×B)/128+(A/255)^2×(255-2B) B>128 则 C=(A×B反相)/128+sqrt(A/255)×(2B-255)
+      softLight: (a, b) => {
+        if (b < 128) {
+          return (
+            ((a * b) / 128) +
+            (Math.pow(a / 255, 2)) * (255 - 2 * b)
+          );
+        } else {
+          return (
+            ((a * reverse(b)) / 128) +
+            (Math.sqrt(a / 255)) * (2 * b - 255)
+          );
+        }
       },
     };
     const cal = (fnc) => {
@@ -159,7 +222,7 @@ export default {
     const handleChange = () => {
       init();
       const form = state.ways[state.selectVal];
-      cal(form)
+      cal(form);
     };
     return {
       ...toRefs(state),
